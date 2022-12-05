6598e29d07a7b6449ba9530504899254d4e6b39b
Deyvi E. Jara Garcia <deyvi.jara@rappi.com>
2022-12-05 16:29:44 -0500

    Configurando Jetstream

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\{User, Category, Course, Post};
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Deyvi Jara Garcia',
+            'email' => 'djara@dev.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now(),            
+            'remember_token' => Str::random(10)
+        ]);
+        User::factory(4)->create();
+        Category::factory(3)->create();
+        Course::factory(10)->create();
+        Post::factory(90)->create();
     }
 }
